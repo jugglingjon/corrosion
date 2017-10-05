d4326a1d7ba1f70f7b982da2d2c28665f3b6d19e
Jonathan Cronin <cronin_jonathan@bah.com>
2017-10-05 13:39:42 -0400

    2.2 adds support for extra small phones (<300px)

--- js/main.js
+++ js/main.js
@@ -21,7 +21,7 @@ var $data,
 	$localKey='corrosionTriviaData',
 	$currentScreen='screen-categories',
 	$lastScreen='screen-categories',
-	$version='2.1';
+	$version='2.2';
 
 var $startTime,
 	$questionTimer,
